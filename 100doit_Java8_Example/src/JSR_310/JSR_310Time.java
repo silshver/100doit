f381554f3867fd39d375257f1ab8c411e4ab98fc
silshver <silshver@gmail.com>
2018-04-24 10:37:18 +0900

    java8

--- 100doit_Java8_Example/src/JSR_310/JSR_310Time.java
+++ 100doit_Java8_Example/src/JSR_310/JSR_310Time.java
@@ -16,6 +16,11 @@ import java.time.zone.ZoneRulesException;
 import org.junit.Test;
 
 public class JSR_310Time {  
+	
+	//http://d2.naver.com/helloworld/645609
+	/*
+	 *  1582년 10월 4일의 하루 뒤를 계산하는 테스트
+	 */
 	@Test
 	public void shouldGetAfterOneDay() {
 		LocalDate theDay = IsoChronology.INSTANCE.date(1582, 10, 4);
@@ -26,20 +31,9 @@ public class JSR_310Time {
 		assertThat(nextDay.format(formatter)).isEqualTo("1582.10.05");
 	}
 	
-	@Test
-	public void shouldGetAfterOneHour() {
-		ZoneId seoul = ZoneId.of("Asia/Seoul");
-		ZonedDateTime theTime = ZonedDateTime.of(1988, 5, 7, 23, 0, 0, 0, seoul);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
-		assertThat(theTime.format(formatter)).isEqualTo("1988.05.07 23:00");
-		ZoneRules seoulRules = seoul.getRules();
-		assertThat(seoulRules.isDaylightSavings(Instant.from(theTime))).isFalse();
-
-		ZonedDateTime after1Hour = theTime.plusHours(1);
-		assertThat(after1Hour.format(formatter)).isEqualTo("1988.05.08 01:00");
-		assertThat(seoulRules.isDaylightSavings(Instant.from(after1Hour))).isTrue();
-	}
-	
+	/*
+	 * 1961년 8월 9일의 1분후를 계산하는 테스트
+	 */
 	@Test
 	public void shouldGetAfterOneMinute() {
 		ZoneId seoul = ZoneId.of("Asia/Seoul");
@@ -50,7 +44,10 @@ public class JSR_310Time {
 		ZonedDateTime after1Minute = theTime.plusMinutes(1);
 		assertThat(after1Minute.format(formatter)).isEqualTo("1961.08.10 00:30");
 	}
-
+	
+	/*
+	 * 2012년 6월 30일 마지막날의 2초후를 계산하는 테스트
+	 */
 	@Test
 	public void shouldGetAfterTwoSecond() {
 		ZoneId utc = ZoneId.of("UTC");
@@ -62,11 +59,17 @@ public class JSR_310Time {
 		assertThat(after2Seconds.format(formatter)).isEqualTo("2012.07.01 00:00:01");
 	}
 
+	/*
+	 * 
+	 */
 	@Test(expected=ZoneRulesException.class)
 	public void shouldThrowExceptionWhenWrongTimeZoneId(){
 		ZoneId.of("Seoul/Asia");
 	}
 	
+	/*
+	 * 날짜의 년 월일을 get하는 테스트
+	 */
 	@Test
 	public void shouldGetDate() {
 		LocalDate theDay = LocalDate.of(1999, 12, 31);
@@ -76,12 +79,12 @@ public class JSR_310Time {
 		assertThat(theDay.getDayOfMonth()).isEqualTo(31);		
 	}
 	
+	
 	@Test(expected=DateTimeException.class)
 	public void shouldNotAcceptWrongDate() {
 		LocalDate.of(1999, 13, 31);
 	}
 
-	
 	@Test
 	public void shouldGetDayOfWeek() {
 		LocalDate theDay = LocalDate.of(2014, 1, 1);
